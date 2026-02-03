eb321ac640d8ccc6cd3d830b332846b554679f61
Stephen <stevmcgin@gmail.com>
2026-02-02 19:19:14 -0500

    New update to scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-const editProfileBtn = document.querySelector(".profile-edit-btn");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
